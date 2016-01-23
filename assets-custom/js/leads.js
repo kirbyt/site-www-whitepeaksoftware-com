544f0e4f955c9dda3ba193ce1e6d0ec5e419b652
Kirby Turner <kirby@thecave.com>
2016-01-23 09:46:44 -0500

    Site updated at 2016-01-23 14:46:44 UTC

--- assets-custom/js/leads.js
+++ assets-custom/js/leads.js
@@ -3,20 +3,50 @@ $(document).ready(function(){
   // Initialize Parse with your Parse application & javascript keys
   Parse.initialize("GTJigWze7dImWIm8OtrF56mMqedj988I0Aj5KGFk", "c8x5Oh9wDw9q9tvnnD7axIEsY0faovZI6H2lVQy3");
 
-  // Setup the form to watch for the submit event
-  $('#formLead').submit(function(e){
-    e.preventDefault();
+  // capture all enter and do nothing
+  $('#email').keypress(function(e) {
+    if(e.which == 13) {
+      $('#email').trigger('focusout');
+      return false;
+    }
+  });
 
+  // capture clicks on validate and do nothing
+  $("#formLead").click(function() {
+    return false;
+  });
+
+  $('#email').mailgun_validator({
+    api_key: 'pubkey-9bfb12cd8a0745f9ee4c4f6fe31bdad8', // replace this with your Mailgun public API key
+    in_progress: validation_in_progress,
+    success: validation_success,
+    error: validation_error,
+  });
+
+  // while the lookup is performing
+  function validation_in_progress() {
     $('#leadResponseSuccess').hide();
     $('#leadResponseError').hide();
+    $('#leadBadEmailAddress').hide();
+    $('#leadBadEmailAddress').html('<strong>Whoops!</strong> You entered an invalid email address.');
+  }
 
-    var email = document.getElementById('email').value
-    if (!email || email.length === 0) {
-      return;
+  // if email successfull validated
+  function validation_success(data) {
+    if (data['is_valid']) {
+      contactLead(data['address']);
+    } else {
+      $('#leadBadEmailAddress').show();
     }
+  }
 
-    // Grab the elements from the form to make up
-    // an object containing name, email and message
+  // if email is invalid
+  function validation_error(error_message) {
+    $('#leadBadEmailAddress').html(error_message);
+    $('#leadBadEmailAddress').show();
+  }
+
+  function contactLead(email) {
     var data = {
       email: email
     }
@@ -33,5 +63,5 @@ $(document).ready(function(){
         $('#leadResponseError').show();
       }
     });
-  });
+  }
 });
