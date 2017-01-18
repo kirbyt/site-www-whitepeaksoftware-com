a87f02f3e5d78bbbca793c0e246866e2992e5271
Kirby Turner <kirby@thecave.com>
2017-01-17 21:58:22 -0500

    Site updated at 2017-01-18 02:58:22 UTC

---
+++ assets-custom/js/new-client-application.js
@@ -0,0 +1,17 @@
+
+var q1oe5vi901khpoh;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'whitepeaksoftware',
+'formHash':'q1oe5vi901khpoh',
+'autoResize':true,
+'height':'1609',
+'async':true,
+'host':'wufoo.com',
+'header':'show',
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { q1oe5vi901khpoh = new WufooForm();q1oe5vi901khpoh.initialize(options);q1oe5vi901khpoh.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');
